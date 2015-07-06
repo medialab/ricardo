950e7411349174514ab7ed3f0fb2112ee3574525
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2015-07-06 17:23:13 +0200

    timeline reimplemented
    the rest: broken

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -17,7 +17,7 @@ angular.module('ricardo.controllers', [])
     ]
 
   })
-  .controller('bilateral', function($scope, $location, reportingEntities) {
+  .controller('bilateral', function($scope, $location, reportingEntities, apiService, DEFAULT_REPORTING, DEFAULT_PARTNER) {
 
     $scope.palette = ["#f1783c", "#b2e5e3", "#3598c0", "#174858"]
     $scope.reportingEntities = reportingEntities;
@@ -73,6 +73,134 @@ angular.module('ricardo.controllers', [])
           $scope.setPagingData($scope.tableData,$scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
         }
     }, true);
+
+    // ADDENDUM from sprint 06 / 07 / 2015
+    // scope.entities.sourceEntity.selected=scope.reportingEntities.filter(function(e){return e.RICid==DEFAULT_REPORTING})[0]
+    // scope.entities.targetEntity.selected=scope.reportingEntities.filter(function(e){return e.RICid==DEFAULT_PARTNER})[0]
+
+    apiService
+      .getFlows({reporting_ids: DEFAULT_REPORTING, partner_ids: DEFAULT_PARTNER})
+      .then(function(data){
+        console.log('Data loaded', data)
+        drawDualTimeline(data.flows)
+      })
+
+    function drawDualTimeline(data){
+      var margin = {top: 10, right: 0, bottom: 30, left: 0},
+          width = document.querySelector('#dual-timeline-container').offsetWidth - margin.left - margin.right,
+          height = 180 - margin.top - margin.bottom;
+
+      var x = d3.time.scale()
+          .range([0, width]);
+
+      var y = d3.scale.linear()
+          .range([height, 0]);
+
+      var xAxis = d3.svg.axis()
+          .scale(x)
+          .orient("bottom");
+
+      var yAxis = d3.svg.axis()
+          .scale(y)
+          .orient("right")
+          .ticks(5)
+          .tickSize(width)
+          .tickFormat(function(d,i){
+            var prefix = d3.formatPrefix(d)
+            if(i == 0){
+              return
+            }
+            else{
+              var symbol;
+              if(prefix.symbol == "G"){
+                symbol = "billion"
+              }else if(prefix.symbol == "M"){
+                symbol = "million"
+              }else if(prefix.symbol == "k"){
+                symbol = "thousand"
+              }else{
+                symbol = ""
+              }
+              return prefix.scale(d) + " " + symbol
+            }
+            
+            })
+
+      var areaImp = d3.svg.area()
+          .x(function(d) { return x(d.date); })
+          .y0(height)
+          .y1(function(d) { return y(d.imp); });
+
+      var areaExp = d3.svg.area()
+          .x(function(d) { return x(d.date); })
+          .y0(height)
+          .y1(function(d) { return y(d.exp); });
+
+      var svg = d3.select("#dual-timeline-container").append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      data.forEach(function(d){
+        d.date = new Date(d.year, 0, 1)
+      })
+
+      x.domain(d3.extent( data, function(d) { return d.date; }));
+      y.domain([0, d3.max( data, function(d) { return Math.max( d.imp, d.exp ); })]);
+
+      svg.append("path")
+          .datum(data)
+          .attr("class", "area-imp")
+          .attr("d", areaImp)
+      
+      svg.append("path")
+          .datum(data)
+          .attr("class", "area-exp")
+          .attr("d", areaExp)
+
+      /* axis */
+
+      var gy = svg.select("g.y.axis"),
+          gx = svg.select("g.x.axis");
+
+      if (svg.select("g.x.axis").empty() || svg.select("g.y.axis").empty()) {
+
+        gx = svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
+
+        gy = svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .call(customAxis);
+            
+        gy.selectAll("g").filter(function(d) { return d; })
+            .classed("minor", true);
+
+      } else {
+
+        gx.transition().duration(duration)
+          .call(xAxis)
+
+        gy.transition().duration(duration)
+          .call(yAxis)
+          .call(customAxis);
+
+        gy.selectAll("g").filter(function(d) { return d; })
+            .classed("minor", true);
+        
+      }
+
+      function customAxis(g) {
+        g.selectAll("text")
+          .attr("x", 4)
+          .attr("dy", -4)
+          .attr("font-size", "0.85em");
+        }
+
+    }
 
   })
 
