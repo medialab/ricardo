7c211b686f95558dc45896022d532ecb4a35c6ad
gtible <gregory.tible@gmail.com>
2015-12-07 12:31:13 +0100

    add translation doc and config

--- client/controllers/translate.controller.js
+++ client/controllers/translate.controller.js
@@ -5,6 +5,7 @@
 angular.module('ricardo.controllers.TranslateController', [])
   .controller('TranslateController', function($translate, $scope) {
     $scope.changeLanguage = function (langKey) {
+    	console.log("langKey", langKey);
       $translate.use(langKey);
     };
   })
