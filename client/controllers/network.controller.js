163bec7e5a5f48ba9553c37a5f45a17680dd2e37
gtible <gregory.tible@gmail.com>
2016-02-08 18:14:30 +0100

    update view and controller network with filters

--- client/controllers/network.controller.js
+++ client/controllers/network.controller.js
@@ -55,6 +55,7 @@ angular.module('ricardo.controllers.network', [])
             edges.push({
                 id: "e" + j,
                 size: size(t.flow),
+                flow: t.flow,
                 source: t.reporting_id,
                 target: t.partner_id
             })
@@ -216,6 +217,147 @@ angular.module('ricardo.controllers.network', [])
         s.startForceAtlas2(LAYOUT_SETTINGS);
     }
 
+    /**
+     * This is an example on how to use sigma filters plugin on a real-world graph.
+     */
+    var filter;
+
+    /**
+     * DOM utility functions
+     */
+    var _ = {
+      $: function (id) {
+        return document.getElementById(id);
+      },
+
+      all: function (selectors) {
+        return document.querySelectorAll(selectors);
+      },
+
+      removeClass: function(selectors, cssClass) {
+        var nodes = document.querySelectorAll(selectors);
+        var l = nodes.length;
+        for ( i = 0 ; i < l; i++ ) {
+          var el = nodes[i];
+          // Bootstrap compatibility
+          el.className = el.className.replace(cssClass, '');
+        }
+      },
+
+      addClass: function (selectors, cssClass) {
+        var nodes = document.querySelectorAll(selectors);
+        var l = nodes.length;
+        for ( i = 0 ; i < l; i++ ) {
+          var el = nodes[i];
+          // Bootstrap compatibility
+          if (-1 == el.className.indexOf(cssClass)) {
+            el.className += ' ' + cssClass;
+          }
+        }
+      },
+
+      show: function (selectors) {
+        this.removeClass(selectors, 'hidden');
+      },
+
+      hide: function (selectors) {
+        this.addClass(selectors, 'hidden');
+      },
+
+      toggle: function (selectors, cssClass) {
+        var cssClass = cssClass || "hidden";
+        var nodes = document.querySelectorAll(selectors);
+        var l = nodes.length;
+        for ( i = 0 ; i < l; i++ ) {
+          var el = nodes[i];
+          //el.style.display = (el.style.display != 'none' ? 'none' : '' );
+          // Bootstrap compatibility
+          if (-1 !== el.className.indexOf(cssClass)) {
+            el.className = el.className.replace(cssClass, '');
+          } else {
+            el.className += ' ' + cssClass;
+          }
+        }
+      }
+    };
+
+
+    function updatePane (graph, filter) {
+      // get max degree
+      var maxDegree = 0,
+          categories = {};
+      
+      // read nodes
+      graph.nodes().forEach(function(n) {
+        console.log("n", n);
+        maxDegree = Math.max(maxDegree, graph.degree(n.id));
+        categories[n.attributes.acategory] = true;
+      })
+
+      console.log("_.$('min-degree')", _.$('min-degree'));
+
+      // min degree
+      if (_.$('min-degree') != null)
+        _.$('min-degree').max = maxDegree;
+      if (_.$('max-degree-value') != null)
+        _.$('max-degree-value').textContent = maxDegree;
+      
+      // node category
+      var nodecategoryElt = _.$('node-category');
+      Object.keys(categories).forEach(function(c) {
+        var optionElt = document.createElement("option");
+        optionElt.text = c;
+        if (nodecategoryElt != null)
+            nodecategoryElt.add(optionElt);
+      });
+
+      // reset button
+      if (_.$('reset-btn') != null) {
+          _.$('reset-btn').addEventListener("click", function(e) {
+            _.$('min-degree').value = 0;
+            _.$('min-degree-val').textContent = '0';
+            _.$('node-category').selectedIndex = 0;
+            filter.undo().apply();
+            _.$('dump').textContent = '';
+            _.hide('#dump');
+          });    
+      }
+
+      // export button
+      if (_.$('export-btn') != null) {
+          _.$('export-btn').addEventListener("click", function(e) {
+            var chain = filter.export();
+            console.log(chain);
+            _.$('dump').textContent = JSON.stringify(chain);
+            _.show('#dump');
+          }); 
+      }
+    }
+
+  
+
+    function applyMinDegreeFilter(e) {
+        var v = e.target.value;
+        _.$('min-degree-val').textContent = v;
+
+        filter
+          .undo('min-degree')
+          .nodesBy(function(n) {
+            return this.degree(n.id) >= v;
+          }, 'min-degree')
+          .apply();
+    }
+
+    function applyCategoryFilter(e) {
+        var c = e.target[e.target.selectedIndex].value;
+        filter
+          .undo('node-category')
+          .nodesBy(function(n) {
+            return !c.length || n.attributes.acategory === c;
+          }, 'node-category')
+          .apply();
+    }
+
     $scope.sigma;
     /* 
      * Calling the API to init country selection
@@ -226,7 +368,6 @@ angular.module('ricardo.controllers.network', [])
           year: year
         })
         .then(function (trades) {
-
             if (trades.length === 0)
                 alert("There is no value for this year")
             else {
@@ -258,22 +399,32 @@ angular.module('ricardo.controllers.network', [])
                 // instantiate graph
             	$scope.sigma  = new sigma(PARAMS);
 
+                // Initialize the Filter API
+                filter = new sigma.plugins.filter($scope.sigma);
+                updatePane($scope.sigma.graph, filter);
+
+                //
                 saveOriginalColor($scope.sigma);
 
                 $scope.sigma.bind('clickNode', function(e) {
-                    console.log("e", e);
+                    
                     var nodeId = e.data.node.id,
                         toKeep = $scope.sigma.graph.neighbors(nodeId);
                     toKeep[nodeId] = e.data.node;
 
+                    $scope.listNations = []
                     $scope.sigma.graph.nodes().forEach(function(n) {
-                      if (toKeep[n.id])
+                      if (toKeep[n.id]) { 
+                        $scope.listNations.push({id: n.id, color: n.color, community: n.community})
                         n.color = n.originalColor;
+                      }
                       else
                         n.color = '#eee';
                     });
 
+
                     $scope.sigma.graph.edges().forEach(function(e) {
+
                       if (toKeep[e.source] && toKeep[e.target])
                         e.color = e.originalColor;
                       else
@@ -284,6 +435,7 @@ angular.module('ricardo.controllers.network', [])
                     // call the refresh method to make the colors
                     // update effective.
                     $scope.sigma.refresh();
+                    $scope.$apply();
                 });
 
                   // When the stage is clicked, we just color each
@@ -326,11 +478,9 @@ angular.module('ricardo.controllers.network', [])
 
                 function stopLayout () {
                     $scope.sigma.stopForceAtlas2();
-                    $scope.sigma.kill()
                 }
 
                 $scope.startLayout = function () {
-                    console.log("start")
                     $scope.sigma.startForceAtlas2(LAYOUT_SETTINGS)
                 }
 
@@ -375,19 +525,30 @@ angular.module('ricardo.controllers.network', [])
                     );
                 }
 
-                $scope.exp = function () {
-                    var expTrades = trades.filter(function (t) {return t.expimp === 'Exp'})
-                    stopLayout();
-                    var data = initGraph(expTrades);
-                    drawGraph($scope.sigma, data);
+                $scope.showNodeOnGraph = function(node) {
+                    //lightening node on graph
+
                 }
 
-                $scope.imp = function () {
-                    var expTrades = trades.filter(function (t) {return t.expimp === 'Imp'})
-                    stopLayout();
-                    var data = initGraph(expTrades);
-                    drawGraph($scope.sigma, data);
-                }
+                // $scope.exp = function () {
+                //     var expTrades = trades.filter(function (t) {return t.expimp === 'Exp'})
+                //     $scope.sigma.stopForceAtlas2();
+                //     $scope.sigma.kill();
+                //     var data = initGraph(expTrades);
+                //     drawGraph($scope.sigma, data);
+                // }
+
+                // $scope.imp = function () {
+                //     var expTrades = trades.filter(function (t) {return t.expimp === 'Imp'})
+                //     $scope.sigma.stopForceAtlas2();
+                //     $scope.sigma.kill();
+                //     var data = initGraph(expTrades);
+                //     drawGraph($scope.sigma, data);
+                // }
+
+                  _.$('min-degree').addEventListener("input", applyMinDegreeFilter);  // for Chrome and FF
+                  _.$('min-degree').addEventListener("change", applyMinDegreeFilter); // for IE10+, that sucks
+                  _.$('node-category').addEventListener("change", applyCategoryFilter);
             }
         })
       }
