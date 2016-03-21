eebb8d4381427877c133d6e8fabc2c968708ddaf
gtible <gregory.tible@gmail.com>
2016-03-21 11:30:21 +0100

    update test on total type

--- database_creation/test/total_type.py
+++ database_creation/test/total_type.py
@@ -11,21 +11,21 @@ def test(cursor):
 		WHERE partner="World_undefined"
 		""")
 	print "%s undefined type of Total Trade to World"%cursor.fetchone()
-	
+
 	cursor.execute("""SELECT count(*),sum(nb) FROM (SELECT count(*) as nb
 		FROM flow_joined
-		group by reporting,Yr,expimp)
+		group by reporting,year,expimp)
 		""")
 	nb_reporting_annual_flows, total_flows=cursor.fetchone()
 	print "%s number of reporting exp or imp total annual flows on %s total"%(nb_reporting_annual_flows,total_flows)
 
-	
+
 	print "\nrepartition of type of Total Trade to World as duplicates:"
-	
+
 	cursor.execute("""SELECT  count(*) FROM (SELECT count(*) as nb,group_concat(partner,"|") as world_type_group
 		FROM flow_joined
 		WHERE partner IN ("World_estimated","World_as_reported")
-		group by reporting,Yr,expimp )
+		group by reporting,year,expimp )
 		""")
 	compatible_world_flows_worldview=cursor.fetchone()[0]
 	print "\n%s %.1f%% flows compatible with world view"%(compatible_world_flows_worldview,100*float(compatible_world_flows_worldview)/nb_reporting_annual_flows)
@@ -33,15 +33,15 @@ def test(cursor):
 	cursor.execute("""SELECT  count(*) FROM (SELECT count(*) as nb,group_concat(partner,"|") as world_type_group
 		FROM flow_joined
 		WHERE partner IN ("World_as_reported","World_as_reported2")
-		group by reporting,Yr,expimp )
+		group by reporting,year,expimp )
 		""")
 	compatible_world_flows_countryview=cursor.fetchone()[0]
 	print "%s %.1f%% flows compatible with world view"%(compatible_world_flows_countryview,100*float(compatible_world_flows_countryview)/nb_reporting_annual_flows)
 
 	cursor.execute("""SELECT  count(*) FROM (SELECT count(*) as nb,group_concat(partner,"|") as world_type_group
 		FROM flow_joined
-		
-		group by reporting,Yr,expimp )
+
+		group by reporting,year,expimp )
 		WHERE world_type_group="World_undefined"
 		group by world_type_group
 		""")
@@ -49,7 +49,7 @@ def test(cursor):
 	print "%s flows to World with only undefined Total"%(undefined[0] if undefined else 0)
 	# missing_world_flows_countryview=0
 	# undefined=0
-	
+
 	# for world_type_group,nb in cursor:
 	# 	print world_type_group,nb
 	# 	if "World_estimated" in world_type_group or "World_as_reported" in world_type_group:
@@ -64,11 +64,11 @@ def test(cursor):
 
 	print "\nisolating World_undefined-only flows :"
 
-	cursor.execute("""SELECT reporting,count(*),group_concat(Yr,"|")
-		FROM (SELECT reporting,Yr,group_concat(partner) as partners_group
+	cursor.execute("""SELECT reporting,count(*),group_concat(year,"|")
+		FROM (SELECT reporting,year,group_concat(partner) as partners_group
 		FROM flow_joined
 		WHERE partner IN ("World_undefined","World_estimated","World_as_reported","World_as_reported2")
-		group by reporting,Yr,expimp )
+		group by reporting,year,expimp )
 		WHERE partners_group="World_undefined"
 		group by reporting
 		""")
