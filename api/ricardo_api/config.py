5d6ebd0846ac01738fd4ff761d20b16a3c78526f
jeremy richard <jeremy.richard@sciencespo.fr>
2019-07-11 18:23:58 +0200

    fix config.py

--- api/ricardo_api/config.py
+++ api/ricardo_api/config.py
@@ -1,6 +1,6 @@
 import os
 DATABASE='RICardo_viz.sqlite'
-DEBUG=os.environ.get('PROD', 'false') === 'false'
-SECRET_KEY=os.environ.get('SECRET_KEY', 'zaeZAEié23123ou"')
-CORS_HEADERS= 'Content-Type'
+DEBUG=os.environ.get('PROD', 'False')
+SECRET_KEY=os.environ.get('SECRET_KEY', 'zaeZAEi23123ou')
+CORS_HEADERS='Content-Type'
 BLOG_RSS='http://ricardo.hypotheses.org/feed'
