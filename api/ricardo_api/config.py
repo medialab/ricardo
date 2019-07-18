70638b95363b992a3985f9c734d33a6a5b465a49
jeremy richard <jeremy.richard@sciencespo.fr>
2019-07-18 16:13:21 +0200

    :whale: docker refacto: api

--- api/ricardo_api/config.py
+++ api/ricardo_api/config.py
@@ -1,6 +1,7 @@
 import os
+import ast
 DATABASE='RICardo_viz.sqlite'
-DEBUG=os.environ.get('PROD', 'False')
+DEBUG=ast.literal_eval(os.environ.get('DEBUG', 'False'))
 SECRET_KEY=os.environ.get('SECRET_KEY', 'zaeZAEi23123ou')
 CORS_HEADERS='Content-Type'
 BLOG_RSS='http://ricardo.hypotheses.org/feed'
