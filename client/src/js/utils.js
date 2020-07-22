2539da9d9936b80ba26e0ee04e8388de8481635d
Alexis Jacomy <alexis.jacomy@gmail.com>
2020-07-22 12:34:46 +0200

    [client] Cleans a bit utils code

--- client/src/js/utils.js
+++ client/src/js/utils.js
@@ -14,7 +14,9 @@ export function initParams($route, $scope, params) {
   params.forEach((param) => {
     if (urlParams[param.name] !== undefined) {
       if (param.list) {
-        $scope[param.name] = param.list.filter((item) => param.getItemId(item) == urlParams[param.name]).shift();
+        $scope[param.name] = param.list
+          .filter((item) => param.getItemId(item) + "" === urlParams[param.name] + "")
+          .shift();
       } else {
         $scope[param.name] = urlParams[param.name];
       }
@@ -23,15 +25,13 @@ export function initParams($route, $scope, params) {
     // Just a watcher to update the url
     const watchlist = params.map((e) => e.name);
     $scope.$watchCollection(`[${watchlist.join(", ")}]`, function (newVal, oldVal) {
-      if (newVal !== oldVal) {
-        newVal.forEach((item, index) => {
-          if (oldVal[index] != newVal[index]) {
-            const param = params.find((e) => e.name == watchlist[index]);
-            urlParams[param.name] = param.getItemId ? param.getItemId(item) : item;
-          }
-        });
-        return $route.updateParams(urlParams);
-      }
+      newVal.forEach((item, index) => {
+        if (oldVal[index] !== newVal[index]) {
+          const param = params.find((e) => e.name === watchlist[index]);
+          urlParams[param.name] = param.getItemId ? param.getItemId(item) : item;
+        }
+      });
+      return $route.updateParams(urlParams);
     });
   });
 }
