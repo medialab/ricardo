d43750206ef97a39f4bb7cd7d4e69d4f634c13e1
Benoit Simard <contact@bsimard.com>
2020-07-23 13:10:54 +0200

    [api]: Correct debug mode with env.

--- api/runserver.py
+++ api/runserver.py
@@ -2,7 +2,7 @@ import os
 from ricardo_api import app
 
 isDebug = False
-if os.environ['FLASK_ENV'] == "development":
+if 'FLASK_ENV' in os.environ and os.environ['FLASK_ENV'] == "development":
     isDebug = True
 
 app.run(host= '0.0.0.0', debug=isDebug)
