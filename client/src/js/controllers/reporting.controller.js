768203b63999a97d92bf8b9dc04ca9a360828bc9
Paul Girard <paul@ouestware.com>
2020-09-28 14:07:02 +0200

    bug in date selection logic

--- client/src/js/controllers/reporting.controller.js
+++ client/src/js/controllers/reporting.controller.js
@@ -341,8 +341,8 @@ angular.module("ricardo.controllers.reporting", []).controller("reporting", [
             return d.year;
           },
         );
-        $scope.selectedMinDate = $scope.rawMinDate;
-        $scope.selectedMaxDate = $scope.rawMaxDate;
+        if (!$scope.selectedMinDate) $scope.selectedMinDate = $scope.rawMinDate;
+        if (!$scope.selectedMaxDate) $scope.selectedMaxDate = $scope.rawMaxDate;
         $scope.rawYearsRange = d3.range($scope.rawMinDate, $scope.rawMaxDate + 1);
         $scope.rawYearsRange_forInf = d3.range($scope.rawMinDate, $scope.selectedMaxDate);
         $scope.rawYearsRange_forSup = d3.range($scope.selectedMinDate + 1, $scope.rawMaxDate + 1);
