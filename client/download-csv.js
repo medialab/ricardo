877479f0ca218794d3117e4cc8cc1d008ae04fe3
Paul Girard <paul@ouestware.com>
2022-04-25 18:07:29 +0200

    [Client] GPH data url changed

--- client/download-csv.js
+++ client/download-csv.js
@@ -2,24 +2,25 @@ const https = require("https");
 const fs = require("fs");
 
 const baseurl = "https://raw.githubusercontent.com/medialab/GeoPolHist/master/data";
-const files = ["GeoPolHist_entities.csv", "GeoPolHist_entities_status_in_time.csv", "GeoPolHist_status.csv"];
+const files = ["GeoPolHist_entities.csv", "GeoPolHist_entities_status_over_time.csv", "GeoPolHist_status.csv"];
 
 files.forEach((filename) => {
   const newfile = `./src/public/csv/${filename}`;
-  if (!fs.existsSync(newfile)) {
-    const file = fs.createWriteStream(newfile);
-
-    console.log(`Download file ${filename}`);
-    const request = https
-      .get(`${baseurl}/${filename}`, (response) => {
-        response.pipe(file);
-        file.on("finish", () => {
-          file.close();
-        });
-      })
-      .on("error", (err) => {
-        fs.unlink(newfile);
-        console.log(err);
+  //if (!fs.existsSync(newfile)) {
+  const file = fs.createWriteStream(newfile);
+  const url = `${baseurl}/${filename}`;
+  console.log(`Download file ${filename} from ${url}`);
+  const request = https
+    .get(url, (response) => {
+      if (response.statusCode !== 200) throw new Error(`HTTP error code ${response.statusCode} for ${url}`);
+      response.pipe(file);
+      file.on("finish", () => {
+        file.close();
       });
-  }
+    })
+    .on("error", (err) => {
+      fs.unlink(newfile);
+      console.log(err);
+    });
+  // }
 });
